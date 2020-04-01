c2ef0d762a282f7d1384f14fcad8e09c7474805a
Tyler Bui-Palsulich <tbp@google.com>
2020-04-01 08:59:39 -0400

    yard: toc, no JS

--- google-cloud-vision/templates_devsite/default/fulldoc/html/setup.rb
+++ google-cloud-vision/templates_devsite/default/fulldoc/html/setup.rb
@@ -1,6 +1,5 @@
 def init
   super
-  puts "Hello from toc\n"
 
   sections.push :toc
 end
@@ -26,7 +25,7 @@ def print_child(out, indent, child, seen)
   seen[child.path] = true
   out << indent << "- title: " << child.path << "\n"
 
-  path = "/ruby/reference/#{child.path}".gsub("::", "/")
+  path = "/ruby/docs/reference/#{child.path}".gsub("::", "/")
   out << indent << "  path: #{path}" << "\n"
   if child.respond_to?(:children)
     children = child.children.select{|c| toc_object(c)}
@@ -34,7 +33,6 @@ def print_child(out, indent, child, seen)
       out << indent << "  section:\n"
       child.children.each do |child|
         if toc_object(child)
-          puts "Go for it", child
           child_seen = print_child(out, indent + "  ", child, seen)
           seen = seen.merge(child_seen)
         end
@@ -46,4 +44,18 @@ end
 
 def toc_object(obj)
   return obj.type != :method && obj.type != :constant
+end
+
+# @return [Array<String>] Stylesheet files that are additionally loaded for the
+#   searchable full lists, e.g., Class List, Method List, File List
+# @since 0.7.0
+def stylesheets_full_list
+  []
+end
+
+# @return [Array<String>] Javascript files that are additionally loaded for the
+#   searchable full lists, e.g., Class List, Method List, File List.
+# @since 0.7.0
+def javascripts_full_list
+  []
 end
---
+++ google-cloud-vision/templates_devsite/default/layout/html/setup.rb
@@ -0,0 +1,11 @@
+# @return [Array<String>] core javascript files for layout
+# @since 0.7.0
+def javascripts
+  []
+end
+
+# @return [Array<String>] core stylesheets for the layout
+# @since 0.7.0
+def stylesheets
+  []
+end
